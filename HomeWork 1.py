421dcf8f92a4bbf3f3bd6bcc711d025ef46c78a0
DisuEpic <136724374+DisuEpic@users.noreply.github.com>
2023-09-28 23:04:03 +0500

    Delete HomeWork 1.py

--- HomeWork 1.py
+++
@@ -1,8 +0,0 @@
-def is_palindrome(string):
-    return string == string[::-1]
-input_string = "лепсспел"
-print(is_palindrome(input_string))  # Вывод: True
-
-input_string = "helloworld"
-print(is_palindrome(input_string))  # Вывод: False
-
